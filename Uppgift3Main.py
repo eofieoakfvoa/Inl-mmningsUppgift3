5ea07ed6f14079e18739d8de95632b3d148a9674
meow <114763852+eofieoakfvoa@users.noreply.github.com>
2025-04-06 05:04:32 +0200

    :cat:

--- Uppgift3Main.py
+++ Uppgift3Main.py
@@ -2,7 +2,11 @@ import Uppgift3Modul
 def Main():
     running = True
     while running:
+        list = None # resettar mellan varje gång
         print("Skriv en ett path till en av dina mappar/katalog/directoyr på din dator för att få en lista på alla filer")
         userInput = input()
-        Uppgift3Modul.GetFilesInDirectory(userInput)
+        list = Uppgift3Modul.GetFilesInDirectory(userInput)
+        if list == None:
+            continue # continue gör så att den skippar allt och början från början i while loopen, detta gör så att allt efter här betyder att listan inte är tom
+        print(list)
 Main()
